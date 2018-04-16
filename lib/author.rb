e921c87a6207bb8d3b3de8ee6380015ccf20a7a6
Marcus Jun <mjun@calstate.edu>
2018-04-16 23:50:21 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author
+  attr_accessor :name
+
+  @@post_count=0
+
+  def initialize(name)
+   @name = name
+   @posts = []
+  end
+
+  def add_post(post)
+   @posts << post
+   post.author = self
+   @@post_count+=1
+ end
+
+  def add_post_by_title(name)#, genre)
+    post = Post.new(name)#, genre)
+    @posts << post
+    post.author = self
+    @@post_count+=1
+    post
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+  def posts
+    @posts
+  end
+end
